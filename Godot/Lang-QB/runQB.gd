c15434b6097be6a3b2b9e83301001feca6dbaf98
Quantum Branching <asample.red@gmail.com>
2026-04-06 08:22:28 -0700

    Updated QB Compiler

--- Godot/Lang-QB/runQB.gd
+++ Godot/Lang-QB/runQB.gd
@@ -1,7 +1,7 @@
 extends TextureButton
 
 var running:bool = false
-var compiler:Assembly
+var asmCompiler:Assembly
 var code:String = ""
 var time:float = 1.0
 var time2:float = 1.0
@@ -13,45 +13,33 @@ func _ready() -> void:
 		if i.ends_with(".qb"):
 			%Edit.text = FileAccess.get_file_as_string(i)
 			code = %Edit.text
-	%Edit.syntax_highlighter = AssemblyHighligher.new(%Edit)
-	compiler = Assembly.new(code)
+	#TODO: %Edit.syntax_highlighter = QBHighligher.new(%Edit)
+	asmCompiler = CompilerQB.compile(code)
 
 func _pressed() -> void:
-	#%Edit.text = CompilerQB.toAssembly(CompilerQB.toPostFix(%Edit.text))
-	%Edit.text = ", ".join(CompilerQB.toPostFix(%Edit.text))
-	return
 	running = !running
 	if running:
 		code = %Edit.text
-		compiler = Assembly.new(code)
+		asmCompiler = CompilerQB.compile(%Edit.text)
 	else:
 		time = 1.0
 		%Edit.text = code
-		compiler = Assembly.new(code)
+		asmCompiler = CompilerQB.compile(%Edit.text)
 
 func _process(delta: float) -> void:
 	if running:
-		compiler.step()
-		
+		asmCompiler.step()
 		time += delta
 		
 		if time > 0.1:
 			if Input.is_key_pressed(KEY_ALT):
-				if %Edit.text != compiler.code:
-					%Edit.text = compiler.code
+				if %Edit.text != asmCompiler.code:
+					%Edit.text = asmCompiler.code
 			else:
-				%Edit.text = compiler.to_string()
+				%Edit.text = asmCompiler.to_string()
 			time = 0.0
 	else:
 		time2 += delta
 		if time2 > 1.0:
-			compiler = Assembly.new(%Edit.text)
+			asmCompiler = Assembly.new(%Edit.text)
 			time2 = 0.0
-	for relative:String in compiler.labels.keys():
-		if relative.begins_with("."):
-			%Edit.add_code_completion_option(CodeEdit.KIND_CONSTANT, relative, relative.replace(".", ""), AssemblyHighligher.RELATIVE)
-		else:
-			%Edit.add_code_completion_option(CodeEdit.KIND_CONSTANT, relative, relative.replace(".", ""), AssemblyHighligher.PRE_ARG)
-	for op in AssemblyHighligher.OPERATIONS:
-		%Edit.add_code_completion_option(CodeEdit.KIND_FUNCTION, op, op, AssemblyHighligher.OP)
-	%Edit.update_code_completion_options(true)
