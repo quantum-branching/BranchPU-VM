d600c7c1439ca4c4cffb8d20e72cfbbd3049a630
Quantum Branching <asample.red@gmail.com>
2026-05-25 15:36:47 -0700

    Renamed Windows.c to Windows.h

--- C/OS/Windows.h
+++ C/OS/Windows.h
@@ -1,4 +1,4 @@
-/// @file Windows.c
+/// @file Windows.h
 /// @brief The Windows-specific system commands for the BPU, which include clearing the screen and printing the screen
 
 #define CLEAR_SCREEN "cls"
